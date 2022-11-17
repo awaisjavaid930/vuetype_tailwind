c2189265ce02673a6023843151b4531c9231690b
Awais Javaid <53362912+awaisjavaid930@users.noreply.github.com>
2022-11-17 17:24:29 +0500

    Starter Kit of VUE, Typescript and Tailwindcss Configure

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# VUE APP, TYPE SCRIPT, TAILWIND CSS CONFIGURE
-This Repositry provide you scaffolding of  Vue App,  Typescript and Tailwindcss 
+# tt-vue
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+      <div class="flex space-x-2 justify-center">
+        <h2 class="text-4xl font-medium leading-tight text-gray-800">
+          Example heading
+          <span class="inline-block py-1.5 px-2.5 leading-none text-center whitespace-nowrap align-baseline font-bold bg-blue-600 text-white rounded">New</span>
+        </h2>
+      </div>
+
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
